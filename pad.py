3297f8c07bdf301005b179b8783a120d032a8dbd
Bartosz Konikiewicz <konikiewiczb@tt.com.pl>
2018-07-10 09:11:42 +0200

    Separate input functions.

--- pad.py
+++ pad.py
@@ -4,23 +4,37 @@ from inputs import get_gamepad
 
 # map BTN_SOUTH to 'A' etc.
 def handle_input(event, value):
-    # print(event, value)
+    if 'BTN' in event:
+        handle_button(event, value)
+    if 'ABS' in event:
+        handle_trackball(event, value)
+
+
+def handle_trackball(event, value):
     max = 32767
-    if event == 'BTN_SOUTH':
-        mouse.click()
-    if event == 'BTN_EAST':
-        mouse.click(button='right')
     if event == 'ABS_X':
         x = (value / max) * 10
-        print(x)
         mouse.move(x, 0, absolute=False)
     if event == 'ABS_Y':
         y = (value / max) * (-10)
         mouse.move(0, y, absolute=False)
 
 
+def handle_button(event, state):
+    if not state:
+        return
+    if event == 'BTN_SOUTH':
+        mouse.click()
+    if event == 'BTN_EAST':
+        mouse.click(button='right')
+
+
 while True:
     for event in get_gamepad():
         # print(event.ev_type)
         handle_input(event.code, event.state)
+        if 'BTN' in event.code:
+            print(event.ev_type)
+
+
 
