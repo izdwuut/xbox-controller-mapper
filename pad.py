2a8c49833a437f9516a831a63033a18c53c44c09
Bartosz Konikiewicz <konikiewiczb@tt.com.pl>
2018-07-13 16:36:59 +0200

    Fix trackball handling to move a mouse pointer.

--- pad.py
+++ pad.py
@@ -3,6 +3,7 @@ from inputs import get_gamepad
 from multiprocessing import Process, Manager
 from constants import BTN_PRESSED, BTN_NOT_PRESSED
 
+
 # map BTN_SOUTH to 'A' etc.
 def handle_input(event, value, prev):
     if 'BTN' in event:
@@ -10,7 +11,7 @@ def handle_input(event, value, prev):
     if 'HAT' in event:
         handle_hat(event, value)
     if 'ABS' in event:
-        handle_trackball(event, value)
+        handle_trackball(event, value, prev)
 
 
 def handle_hat(event, state):
@@ -30,21 +31,29 @@ def handle_hat(event, state):
         mouse.move(0, y, absolute=False)
 
 
-def handle_trackball(event, value):
+# 2 kierunki osi
+def handle_trackball(event, value, prev):
+    if event not in prev:
+        prev[event] = 0.0
     max = 32767
+    ratio = 1
     if event == 'ABS_X':
-        x = (value / max) * 10
-        mouse.move(x, 0, absolute=False)
+        prev['ABS_X'] = prev['ABS_X'] + (value / max / ratio)
+        print(prev['ABS_X'])
+        if prev['ABS_X'] >= 1:
+            mouse.move(1, 0, absolute=False)
+            prev['ABS_X'] = 0
+        elif prev['ABS_X'] <= -1:
+            mouse.move(-1, 0, absolute=False)
+            prev['ABS_X'] = 0
     if event == 'ABS_Y':
-        y = (value / max) * (-10)
-        mouse.move(0, y, absolute=False)
-
-
-        # y = (value / max) * (-10)
-        s[1] = s[1] + (value / max /10000)
-        if s[1] > 1:
-            s[1] = 1
-        mouse.move(0, int(s[1]), absolute=False)
+        prev['ABS_Y'] = prev['ABS_Y'] + (-value / max / ratio)
+        if prev['ABS_Y'] >= 1:
+            mouse.move(0, 1, absolute=False)
+            prev['ABS_Y'] = 0
+        elif prev['ABS_Y'] <= -1:
+            mouse.move(0, -1, absolute=False)
+            prev['ABS_Y'] = 0
 
 
 def handle_button(event, state, prev):
@@ -65,11 +74,8 @@ def handle_button(event, state, prev):
 
 def f(events, prev):
     i = 1
-    # print(events)
     while True:
         i = i + 1
-        # print(events.events)
-        # print(str(i))
         for code, state in events.items():
             handle_input(code, state, prev)
 
@@ -81,18 +87,9 @@ def buffer_input(event, events):
 if __name__ == '__main__':
     e = Manager().dict()
     prev = Manager().dict()
-    # s = Array('d', [0.0, 0.0])
-    # print(e)
     p = Process(target=f, args=(e, prev))
     p.start()
-
     while True:
         for event in get_gamepad():
-            # print(event.ev_type)
-            # handle_input(event.code, event.state)
-            print(event.ev_type, event.code, event.state)
+            # print(event.ev_type, event.code, event.state)
             buffer_input(event, e)
-            # p.join()
-            # print(e)
-
-
