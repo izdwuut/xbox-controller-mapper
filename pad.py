9f1aaebf803754902562ea02ac476252bb284b51
Bartosz Konikiewicz <konikiewiczb@tt.com.pl>
2018-07-13 15:22:50 +0200

    Fix mouse events.

--- pad.py
+++ pad.py
@@ -1,13 +1,12 @@
-import keyboard, inputs, mouse
+import keyboard, mouse
 from inputs import get_gamepad
 from multiprocessing import Process, Manager
-from constants import *
-
+from constants import BTN_PRESSED, BTN_NOT_PRESSED
 
 # map BTN_SOUTH to 'A' etc.
-def handle_input(event, value):
+def handle_input(event, value, prev):
     if 'BTN' in event:
-        handle_button(event, value)
+        handle_button(event, value, prev)
     if 'HAT' in event:
         handle_hat(event, value)
     if 'ABS' in event:
@@ -41,29 +40,38 @@ def handle_trackball(event, value):
         mouse.move(0, y, absolute=False)
 
 
-def handle_button(event, state):
-    if not state:
-        return
-    if event == 'BTN_SOUTH':
-        mouse.click()
-    if event == 'BTN_EAST':
-        mouse.click(button='right')
+        # y = (value / max) * (-10)
+        s[1] = s[1] + (value / max /10000)
+        if s[1] > 1:
+            s[1] = 1
+        mouse.move(0, int(s[1]), absolute=False)
 
 
-class Events:
-    def __init__(self):
-        self.events = {}
+def handle_button(event, state, prev):
+    if state == BTN_PRESSED:
+        if event not in prev or prev[event] == BTN_NOT_PRESSED:
+            if event == 'BTN_SOUTH':
+                mouse.press()
+            if event == 'BTN_EAST':
+                mouse.press(button='right')
+            prev[event] = BTN_PRESSED
+    else:
+        if mouse.is_pressed():
+            mouse.release()
+        if mouse.is_pressed(button='right'):
+            mouse.release(button='right')
+        prev[event] = BTN_NOT_PRESSED
 
 
-def f(events):
+def f(events, prev):
     i = 1
-    print(events)
+    # print(events)
     while True:
         i = i + 1
         # print(events.events)
         # print(str(i))
         for code, state in events.items():
-            handle_input(code, state)
+            handle_input(code, state, prev)
 
 
 def buffer_input(event, events):
@@ -72,17 +80,19 @@ def buffer_input(event, events):
 
 if __name__ == '__main__':
     e = Manager().dict()
-    print(e)
-    p = Process(target=f, args=(e,))
+    prev = Manager().dict()
+    # s = Array('d', [0.0, 0.0])
+    # print(e)
+    p = Process(target=f, args=(e, prev))
     p.start()
 
     while True:
         for event in get_gamepad():
             # print(event.ev_type)
             # handle_input(event.code, event.state)
-            # print(event.ev_type, event.code, event.state)
+            print(event.ev_type, event.code, event.state)
             buffer_input(event, e)
             # p.join()
-            print(e)
+            # print(e)
 
 
