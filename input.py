f5f4ad6152190f9ed69bd9d22698e5c157653931
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-19 22:24:36 +0100

    Add input module.

---
+++ input.py
@@ -0,0 +1,40 @@
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+ULONG_PTR = ctypes.POINTER(ctypes.c_ulong)
+
+
+class KeyboardInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", ULONG_PTR)]
+
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", ULONG_PTR)]
+
+
+class Inputs(ctypes.Union):
+    _fields_ = [("ki", KeyboardInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("inputs", Inputs)]
